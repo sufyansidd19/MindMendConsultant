10c74387bb209434a8a9359f95d79f44459f51b4
sufyansidd19 <112391971+sufyansidd19@users.noreply.github.com>
2023-12-17 02:02:15 +0500

    review

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ btnlogin.addEventListener("click", ShowModal)
 
 var c=document.querySelector('.close');
 c.addEventListener("click", CloseModal)
+
+
+
+function ShowModal1(){
+    document.querySelector('.overlay1').classList.add('showoverlay1');
+    document.querySelector('.bookform').classList.add('showbookform');
+}
+function CloseModal(){
+    document.querySelector('.overlay1').classList.remove('showoverlay1');
+    document.querySelector('.bookform').classList.remove('showbookform');
+}
+var btnlogin=document.querySelector('.btn-login1');
+btnlogin.addEventListener("click", ShowModal)
+
+var c=document.querySelector('.close1');
+c.addEventListener("click", CloseModal)
