c28644756f3ab2f874fb0398a3a5e56cd05d2f7c
sufyansidd19 <112391971+sufyansidd19@users.noreply.github.com>
2023-12-17 13:23:40 +0500

    payment

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ function ShowModal1(){
     document.querySelector('.overlay1').classList.add('showoverlay1');
     document.querySelector('.bookform').classList.add('showbookform');
 }
-function CloseModal(){
+function CloseModal1(){
     document.querySelector('.overlay1').classList.remove('showoverlay1');
     document.querySelector('.bookform').classList.remove('showbookform');
 }
-var btnlogin=document.querySelector('.btn-login1');
-btnlogin.addEventListener("click", ShowModal)
+var btnlogin1=document.querySelector('.btn-login1');
+btnlogin1.addEventListener("click", ShowModal1)
 
-var c=document.querySelector('.close1');
-c.addEventListener("click", CloseModal)
+var c2=document.querySelector('.close1');
+c2.addEventListener("click", CloseModal1)
