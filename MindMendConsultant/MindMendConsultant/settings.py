6cf73f157ee20fcf16f4418527c1b0d4e5a04f9e
sufyansidd19 <112391971+sufyansidd19@users.noreply.github.com>
2023-12-25 17:19:09 +0500

    media root

--- MindMendConsultant/MindMendConsultant/settings.py
+++ MindMendConsultant/MindMendConsultant/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 
-MEDIA_ROOT=os.path.join(BASE_DIR,"medium","images")
+MEDIA_ROOT=os.path.join(BASE_DIR,'public','static')
 MEDIA_URL='/media/'
 
 # Default primary key field type
