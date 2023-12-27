50439fa4342c390afe5b7320b50c613794d0ef7e
MohibShaikh <mohibuddin9@gmail.com>
2023-12-27 12:42:33 +0500

    Add files via upload

--- MindMendConsultant/static/app.js
+++ MindMendConsultant/static/app.js
@@ -1,29 +1,29 @@
-function ShowModal(){
-    document.querySelector('.overlay').classList.add('showoverlay');
-    document.querySelector('.loginform').classList.add('showloginform');
+function showModal(overlayClass, formClass) {
+    document.querySelector(`.${overlayClass}`).classList.add('showoverlay');
+    document.querySelector(`.${formClass}`).classList.add('showloginform');
 }
-function CloseModal(){
-    document.querySelector('.overlay').classList.remove('showoverlay');
-    document.querySelector('.loginform').classList.remove('showloginform');
+
+function closeModal(overlayClass, formClass) {
+    document.querySelector(`.${overlayClass}`).classList.remove('showoverlay');
+    document.querySelector(`.${formClass}`).classList.remove('showloginform');
 }
-var btnlogin=document.querySelector('.btn-login');
-btnlogin.addEventListener("click", ShowModal)
 
-var c=document.querySelector('.close');
-c.addEventListener("click", CloseModal)
+var btnLogin = document.querySelector('.btn-login');
+btnLogin.addEventListener("click", function () {
+    showModal('overlay', 'loginform');
+});
 
+var closeBtn = document.querySelector('.close');
+closeBtn.addEventListener("click", function () {
+    closeModal('overlay', 'loginform');
+});
 
+var btnLogin1 = document.querySelector('.btn-login1');
+btnLogin1.addEventListener("click", function () {
+    showModal('overlay1', 'bookform');
+});
 
-function ShowModal1(){
-    document.querySelector('.overlay1').classList.add('showoverlay1');
-    document.querySelector('.bookform').classList.add('showbookform');
-}
-function CloseModal1(){
-    document.querySelector('.overlay1').classList.remove('showoverlay1');
-    document.querySelector('.bookform').classList.remove('showbookform');
-}
-var btnlogin1=document.querySelector('.btn-login1');
-btnlogin1.addEventListener("click", ShowModal1)
-
-var c2=document.querySelector('.close1');
-c2.addEventListener("click", CloseModal1)
+var closeBtn1 = document.querySelector('.close1');
+closeBtn1.addEventListener("click", function () {
+    closeModal('overlay1', 'bookform');
+});
